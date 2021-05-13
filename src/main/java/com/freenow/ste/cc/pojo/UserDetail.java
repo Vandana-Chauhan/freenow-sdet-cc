d91ac28100784cc7f924fd3ef38006489b32badd
b0211497 <b0211497@localhost>
2021-05-13 21:45:19 +0530

    Added User POJO

---
+++ src/main/java/com/freenow/ste/cc/pojo/UserDetail.java
@@ -0,0 +1,118 @@
+package com.freenow.ste.cc.pojo;
+
+
+
+public class UserDetail {
+	
+	
+	private static String id;
+	private static String name;
+	private static String username;
+	private static String email;
+	private static Address address;
+	private static String phone;
+	private static String website;
+	private static Company company;
+	/**
+	 * @return the id
+	 */
+	public  String getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public  void setId(String id) {
+		UserDetail.id = id;
+	}
+	/**
+	 * @return the name
+	 */
+	public  String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public  void setName(String name) {
+		UserDetail.name = name;
+	}
+	/**
+	 * @return the username
+	 */
+	public  String getUsername() {
+		return username;
+	}
+	/**
+	 * @param username the username to set
+	 */
+	public  void setUsername(String username) {
+		UserDetail.username = username;
+	}
+	/**
+	 * @return the email
+	 */
+	public  String getEmail() {
+		return email;
+	}
+	/**
+	 * @param email the email to set
+	 */
+	public  void setEmail(String email) {
+		UserDetail.email = email;
+	}
+	/**
+	 * @return the address
+	 */
+	public  Address getAddress() {
+		return address;
+	}
+	/**
+	 * @param address the address to set
+	 */
+	public  void setAddress (Address address) {
+		UserDetail.address = address;
+	}
+	/**
+	 * @return the phone
+	 */
+	public  String getPhone() {
+		return phone;
+	}
+	/**
+	 * @param phone the phone to set
+	 */
+	public  void setPhone(String phone) {
+		UserDetail.phone = phone;
+	}
+	/**
+	 * @return the website
+	 */
+	public  String getWebsite() {
+		return website;
+	}
+	/**
+	 * @param website the website to set
+	 */
+	public  void setWebsite(String website) {
+		UserDetail.website = website;
+	}
+	/**
+	 * @return the company
+	 */
+	public  Company getCompany() {
+		return company;
+	}
+	/**
+	 * @param company the company to set
+	 */
+	public  void setCompany(Company company) {
+		UserDetail.company = company;
+	}
+	
+	
+	
+	
+	
+	
+}
