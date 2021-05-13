9c1562c113a93ec7aa40e5616b1da355c5dce085
b0211497 <b0211497@localhost>
2021-05-13 21:46:40 +0530

    Added test runner to execute test cases

---
+++ src/test/java/com/freenow/ste/cc/test/runner/TestRunner.java
@@ -0,0 +1,13 @@
+package com.freenow.ste.cc.test.runner;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/java/com/freenow/ste/cc/features", glue = {
+		"com/freenow/ste/cc/step/definations" }, plugin = "json:target/jsonReports/free-nowTestReport.json")
+public class TestRunner {
+
+}
